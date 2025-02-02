dda74043f5d375c3042d113638223a8605a496bd
Hamid Qureshi <hamidq786@hotmail.com>
2025-02-02 23:02:07 +0000

    added handling exceptions

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -1,11 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import 'semantic-ui-css/semantic.min.css'
-import './app/layout/styles.css'
-import 'react-calendar/dist/Calendar.css'
-import { store, StoreContext } from './app/stores/store.ts'
-import { RouterProvider } from 'react-router-dom'
-import { router } from './app/router/Routes.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import 'semantic-ui-css/semantic.min.css';
+import 'react-toastify/ReactToastify.css';
+import './app/layout/styles.css';
+import 'react-calendar/dist/Calendar.css';
+import { store, StoreContext } from './app/stores/store.ts';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './app/router/Routes.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
