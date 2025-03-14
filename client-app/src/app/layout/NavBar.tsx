9019e5fe7d941424f77b1fce5455801d144c59a2
Hamid Qureshi <hamidq786@hotmail.com>
2025-03-14 17:02:09 +0000

    Handling validation errors in register form

--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -1,21 +1,34 @@
-import { Button, Container, Menu, MenuItem } from "semantic-ui-react";
-import { NavLink } from "react-router-dom";
+import { Button, Container, Menu, MenuItem, Image, Dropdown, DropdownItem, DropdownMenu } from "semantic-ui-react";
+import { Link, NavLink } from "react-router-dom";
+import { useStore } from "../stores/store";
+import { observer } from "mobx-react-lite";
 
-export default function NavBar() {
-
+export default observer(function NavBar() {
+    const { userStore: { user, logout } } = useStore();
     return (
         <Menu inverted fixed='top'>
             <Container>
                 <MenuItem as={NavLink} to='/' header>
-                    <img src="/assets/logo.png" alt="logo" style={{marginRight: '10px'}}/>
+                    <img src="/assets/logo.png" alt="logo" style={{ marginRight: '10px' }} />
                     Reactivities
                 </MenuItem>
-                <MenuItem as={NavLink} to='/activities' name='Activities'/>
-                <MenuItem as={NavLink} to='/errors' name='Errors'/>
+                <MenuItem as={NavLink} to='/activities' name='Activities' />
+                <MenuItem as={NavLink} to='/errors' name='Errors' />
                 <MenuItem>
-                    <Button as={NavLink} to='/createActivity' positive content='Create Activity'/>
+                    <Button as={NavLink} to='/createActivity' positive content='Create Activity' />
+                </MenuItem>
+                <MenuItem position="right">
+                    <Image src={user?.image || "/assets/user.png"} avatar spaced="right" />
+                    <Dropdown pointing="top left" text={user?.displayName}>
+                        <DropdownMenu>
+                            <DropdownItem as={Link} to={`/profile/${user?.username}`}
+                                text="My Profile"
+                                icon="user" />
+                            <DropdownItem onClick={logout} text="logout" icon="power" />
+                        </DropdownMenu>
+                    </Dropdown>
                 </MenuItem>
             </Container>
         </Menu>
     )
-}
+})
